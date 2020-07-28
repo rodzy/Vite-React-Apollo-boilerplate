ac52f892887d27494f45d30ef6a8bce50bcf1066
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-27 19:52:45 -0600

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, lazy } from "react";
 import graphql from "./static/graphql.svg";
 import react from "./static/react.svg";
 import "./styles/App.css";
+import { Link, StaticRouter, Switch, Route } from "react-router-dom";
+import Page2 from "./pages/page-2";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-logos">
-          <img src={react} className="App-logo" alt="logo" />
-          <img src={graphql} className="App-logo-gql" alt="logo" />
-        </div>
-        <p>Hello Vite + React + Apollo</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logos">
+            <img src={react} className="App-logo" alt="logo" />
+            <img src={graphql} className="App-logo-gql" alt="logo" />
+          </div>
+          <p>Hello Vite + React + Apollo</p>
+          <Link to="/pdos">Go to page 2</Link>
+          <p>
+            Edit <code>App.jsx</code> and save to test HMR updates.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <Switch>
+        <Route path="/pdos">
+          <Page2 />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
