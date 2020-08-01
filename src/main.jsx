8c2cbb43b0db918fe04b893ce6e87e8e24618508
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-01 16:50:39 -0600

    Set up for the Apollo Client/Providers on DOM render

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
+/* Set up your client from your backend server
+   this (uri) param sets up your backend Apollo server
+   for the client to receive data, in this case is the
+   localhost defined from my other example: 
+    https://github.com/rodzy/graphql-apollo-starter.
+*/
+const client = new ApolloClient({
+  uri: "https://localhost:8080/graphql",
+});
+
+/* The Apollo Provider wraps all the SPA App
+   to give access to the API data flow inside 
+   the DOM
+*/
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
