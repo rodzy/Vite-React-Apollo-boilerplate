5a94b22bd7ed99de692598ee15bc6b3b6921396a
rodzy <49137701+rodzy@users.noreply.github.com>
2020-08-03 20:59:20 -0600

    Added miscelaneous pages

--- src/pages/page-2.jsx
+++ src/pages/page-2.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import '../styles/Page-2.css'
 
 const Page2 = () => {
-    return ( 
-        <h1>Hi</h1>
-     );
-}
- 
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Fetch users</h2>
+        <button className="page-2-button">Click to fetch</button>
+      </div>
+    </div>
+  );
+};
+
 export default Page2;
